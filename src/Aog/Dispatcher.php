24300651329c1a834c89aad78813d197394ce48a
Jeong Jimin <k154085@kansai-u.ac.jp>
2026-09-03 17:24:54 +0900

    Match NIMA start score with Python reference

--- src/Aog/Dispatcher.php
+++ src/Aog/Dispatcher.php
@@ -12,7 +12,7 @@ final class Dispatcher
     private const GAME_MODES = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
     private const GMODE_TAKU = [1=>0,2=>1,3=>2,4=>3,5=>0,6=>0,7=>2,8=>0,9=>0,10=>2,11=>0,12=>2,13=>0,14=>2,15=>0,16=>0,17=>2,18=>2,19=>2,20=>0,21=>2,22=>0,23=>2];
     private const SEATS = [0=>4,1=>4,2=>3,3=>2];
-    private const START_SCORE = [0=>25000,1=>25000,2=>35000,3=>50000];
+    private const START_SCORE = [0=>25000,1=>25000,2=>35000,3=>35000];
     private const BASE_EVENTS = [
         'SpiritGymBonusEvent','ConstancyFireReach','ConstancyFireReachAppearance',
         'ConstancyAccelDora','ConstancyAccelDoraSchedule','ConstancyMentanpin',
